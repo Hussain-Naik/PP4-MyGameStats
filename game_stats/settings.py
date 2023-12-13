48420ca5c36eccc31f18c1aae42ee4c2fdd851ab
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-12-13 09:30:33 +0000

    Add events, profile, friends django app

--- game_stats/settings.py
+++ game_stats/settings.py
@@ -47,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'matchups',
+    'events',
+    'friends',
+    'profiles',
 ]
 
 MIDDLEWARE = [
