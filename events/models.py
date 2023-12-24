6ef56c1ed486f7895a448314d7a26be27af0b291
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-12-24 10:12:10 +0000

    Add junction table through sessions model

--- events/models.py
+++ events/models.py
@@ -6,24 +6,15 @@ class Group(models.Model):
     """Group event model"""
     name = models.CharField(max_length=30, unique=True, blank=False)
     slug = models.SlugField(unique=True, blank=False)
-    members = models.ManyToManyField(User, related_name='group', through='Players')
     admin = models.ForeignKey(
         User, on_delete=models.DO_NOTHING, related_name='group_admin')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    private_group = models.BooleanField(default=False)
 
     def __str__(self):
         """Events model string representation"""
         return self.name
-
-
-class Players(models.Model):
-    """Associated model for group and users"""
-    member = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    playing = models.BooleanField(default=False)
-    wins = models.IntegerField(default=0)
-    updated_on = models.DateTimeField(auto_now=True)
 
 class Sessions(models.Model):
     STATUS = (
@@ -31,16 +22,21 @@ class Sessions(models.Model):
     (2, 'in progress'),
     (3, 'completed'),
     )
+    GAME_TYPE = ((1, 'Singles'),(2, 'Doubles'))
+    TEAM_TYPE = ((1, 'Random'),(2, 'User Defined'))
     """Event session model"""
     name = models.CharField(max_length=30, unique=True, blank=False)
     group = models.ForeignKey(
         Group, on_delete=models.CASCADE, related_name='session')
+    players = models.ManyToManyField(User, through='PlayerSessions', related_name='sessions')
     admin = models.ForeignKey(
         User, on_delete=models.DO_NOTHING, related_name='session_admin')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     joinable = models.BooleanField(default=True)
     status = models.IntegerField(choices=STATUS, default=1)
+    game_type = models.IntegerField(choices=GAME_TYPE, default=1)
+    team_selection = models.IntegerField(choices=TEAM_TYPE, default=1)
 
     def __str__(self):
         """Sessions model string representation"""
@@ -48,23 +44,15 @@ class Sessions(models.Model):
     
     @property
     def player_count(self):
-        return self.group.members.count()
+        return self.players.count()
     
-class SessionScore(models.Model):
-    """Session score model"""
-    player_id = models.IntegerField(default=0, null=False, blank=False)
-    inc = models.IntegerField(default=0)
-    player_score = models.IntegerField(default=0)
-    session = models.ForeignKey(
-        Sessions, on_delete=models.CASCADE, related_name='session_scores')
-    
-    @property
-    def name(self):
-        return 'Player '+ self.inc
-
-    def __str__(self):
-        """Session score model string representation"""
-        return self.name
+class PlayerSessions(models.Model):
+    """Associated model for group and users"""
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(Sessions, on_delete=models.CASCADE)
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    updated_on = models.DateTimeField(auto_now=True)
 
 
 class Game(models.Model):
@@ -75,28 +63,16 @@ class Game(models.Model):
     admin = models.ForeignKey(
         User, on_delete=models.DO_NOTHING, related_name='game_admin')
     team_1_score = models.IntegerField(default=0)
+    team_1_player1 = models.IntegerField(blank=False, null=False)
+    team_1_player2 = models.IntegerField(blank=True, null=True)
     team_2_score = models.IntegerField(default=0)
+    team_2_player1 = models.IntegerField(blank=False, null=False)
+    team_2_player2 = models.IntegerField(blank=True, null=True)
 
     @property
     def name(self):
         return 'Game '+ self.inc
     
-    @property
-    def team1_player1(self):
-        return 1
-    
-    @property
-    def team1_player2(self):
-        return 2
-    
-    @property
-    def team2_player1(self):
-        return 3
-    
-    @property
-    def team2_player2(self):
-        return 4
-
     def __str__(self):
         """Game model string representation"""
         return self.name
