5106f2d2dcbaac51a93cc6365e5780a3a162ec1e
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-04-16 09:59:24 +0100

    Add debug database

--- badminton/settings.py
+++ badminton/settings.py
@@ -86,14 +86,20 @@ WSGI_APPLICATION = 'badminton.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
+else:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
-
+print(DEBUG)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
