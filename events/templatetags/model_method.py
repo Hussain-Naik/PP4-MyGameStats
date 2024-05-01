efde462ef21358cef095049c7869a47a9e718164
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-05-01 09:55:22 +0100

    Add pep8 validation

--- events/templatetags/model_method.py
+++ events/templatetags/model_method.py
@@ -1,13 +1,17 @@
+'''template tags for passing variables in method'''
 from django import template
 register = template.Library()
+
 
 @register.filter(name='grp_session_wins')
 def call_method(obj, grp):
     return obj.profile.get_session_wins(grp)
 
+
 @register.filter(name='grp_game_wins')
 def call_method(obj, grp):
     return obj.profile.get_group_game_wins(grp)
+
 
 @register.filter(name='profile_pairing')
 def call_method(obj, profile):
