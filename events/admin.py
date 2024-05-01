13e8ce7abdfc1714c680050cbe758176f58e7210
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-05-01 07:45:11 +0100

    Add pep8 validation

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,8 @@
+'''Admin for the Events app'''
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+
 admin.site.register(Group)
 admin.site.register(Session)
 admin.site.register(Roster)
