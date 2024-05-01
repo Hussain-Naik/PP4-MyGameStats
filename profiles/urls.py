7e0b02a555aa156f8c7251c1780afd6feeedaa02
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-05-01 18:47:47 +0100

    Add pep8 validation

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,3 +1,4 @@
+'''urls for profile app'''
 from django.urls import path
 from . import views
 
@@ -6,11 +7,39 @@ urlpatterns = [
     path('update/', views.UpdateProfile.as_view(), name="update_profile"),
     path('search/', views.FriendSearchView.as_view(), name="search_profile"),
     path('<pk>/', views.UserProfileView.as_view(), name="user_profile"),
-    path('<pk>/remove/', views.RemoveFriendView.as_view(), name="remove_profile"),
-    path('request/search/', views.FriendRequestListView.as_view(), name="list_request"),
-    path('request/<pk>/update/', views.UpdateFriendRequestView.as_view(), name="update_request"),
-    path('request/<pk>/delete/', views.DeleteFriendRequestView.as_view(), name="delete_request"),
-    path('session/search/', views.SessionRequestListView.as_view(), name="list_invites"),
-    path('session/<pk>/update/', views.UserSessionInviteView.as_view(), name="update_invites"),
-    path('session/<pk>/delete/', views.DeleteUserSessionInviteView.as_view(), name="delete_invites"),
+    path(
+        '<pk>/remove/',
+        views.RemoveFriendView.as_view(),
+        name="remove_profile"
+        ),
+    path(
+        'request/search/',
+        views.FriendRequestListView.as_view(),
+        name="list_request"
+        ),
+    path(
+        'request/<pk>/update/',
+        views.UpdateFriendRequestView.as_view(),
+        name="update_request"
+        ),
+    path(
+        'request/<pk>/delete/',
+        views.DeleteFriendRequestView.as_view(),
+        name="delete_request"
+        ),
+    path(
+        'session/search/',
+        views.SessionRequestListView.as_view(),
+        name="list_invites"
+        ),
+    path(
+        'session/<pk>/update/',
+        views.UserSessionInviteView.as_view(),
+        name="update_invites"
+        ),
+    path(
+        'session/<pk>/delete/',
+        views.DeleteUserSessionInviteView.as_view(),
+        name="delete_invites"
+        ),
 ]
