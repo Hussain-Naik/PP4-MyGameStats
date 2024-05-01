4ac1e24baa0b27e951e67ccdefae26c03f776e71
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-05-01 19:16:21 +0100

    Add pep8 validation

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+'''Views for profile app'''
 from django.contrib.auth.mixins import AccessMixin
 from django.views.generic.edit import UpdateView, FormMixin, DeleteView
 from django.views.generic import DetailView, ListView, TemplateView
@@ -8,8 +9,8 @@ from .forms import *
 from .models import *
 
 
-# Create your views here.
 class MyProfileView(LoginRequiredMixin, DetailView):
+    '''Profile view for current user'''
     model = Profile
     template_name = 'profiles/profile.html'
     context_object_name = 'user_object'
@@ -19,6 +20,7 @@ class MyProfileView(LoginRequiredMixin, DetailView):
 
 
 class UpdateProfile(LoginRequiredMixin, UpdateView):
+    '''Update profile view'''
     model = Profile
     form_class = CustomUserChangeForm
     template_name = 'home/form.html'
@@ -26,13 +28,16 @@ class UpdateProfile(LoginRequiredMixin, UpdateView):
 
     def get_object(self):
         return get_object_or_404(Profile, user__id=self.request.user.id)
-    
+
     def get_context_data(self, **kwargs):
+        '''Add name to form'''
         context = super().get_context_data(**kwargs)
         context['page'] = 'User Update Form'
         return context
 
+
 class UserProfileView(FormMixin, DetailView):
+    '''Profile view for other users'''
     model = Profile
     template_name = 'profiles/profile.html'
     context_object_name = 'user_object'
@@ -40,58 +45,66 @@ class UserProfileView(FormMixin, DetailView):
 
     def get_success_url(self):
         return reverse_lazy("user_profile", kwargs={"pk": self.kwargs['pk']})
-    
+
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
-      
+
     def form_valid(self, form):
         receiver_ = Profile.objects.get(id=self.kwargs['pk'])
         sender_ = Profile.objects.get(user__id=self.request.user.id)
         print(sender_, receiver_)
-        FriendRequest.objects.get_or_create(sender=sender_, receiver=receiver_, status='pending')
+        FriendRequest.objects.get_or_create(
+            sender=sender_,
+            receiver=receiver_,
+            status='pending'
+            )
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated and FriendRequest.objects.filter(
-              sender= Profile.objects.get(user__id=self.request.user.id), 
-              receiver=Profile.objects.get(id=self.kwargs['pk'])
-          ).exists():
+                sender=Profile.objects.get(user__id=self.request.user.id),
+                receiver=Profile.objects.get(id=self.kwargs['pk'])).exists():
             context['sendRequest'] = FriendRequest.objects.get(
-                sender= Profile.objects.get(user__id=self.request.user.id), 
+                sender=Profile.objects.get(user__id=self.request.user.id),
                 receiver=Profile.objects.get(id=self.kwargs['pk'])
-          )
+                )
         else:
-          context['sendRequest'] = False
+            context['sendRequest'] = False
         return context
 
+
 class FriendSearchView(ListView):
+    '''Profile list view'''
     model = Profile
     template_name = 'profiles/profile_list.html'
     context_object_name = 'list_object'
     paginate_by = 12
 
     def get_queryset(self):
-        q = self.request.GET.get('q') if self.request.GET.get('q') != None else ''
+        q = self.request.GET.get(
+            'q'
+            ) if self.request.GET.get('q') is not None else ''
         queryset = Profile.objects.filter(
-            Q(first_name__icontains=q) | 
-            Q(last_name__icontains=q) | 
+            Q(first_name__icontains=q) |
+            Q(last_name__icontains=q) |
             Q(user__username__icontains=q)
         )
         return queryset
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search'] = True
         return context
 
+
 class UpdateFriendRequestView(AccessMixin, UpdateView):
+    '''Update friend request view'''
     model = FriendRequest
-    
     template_name = 'home/form.html'
     success_url = reverse_lazy('profile')
 
@@ -99,19 +112,16 @@ class UpdateFriendRequestView(AccessMixin, UpdateView):
         form_class = UpdateFriendRequestForm
         if self.get_object().sender.user == self.request.user:
             form_class = UpdateFriendRequestSentForm
-        
         return form_class
 
     def dispatch(self, request, *args, **kwargs):
         profile = Profile.objects.get(user__id=self.request.user.id)
-        print(self.get_object().receiver != profile, self.get_object().sender == profile)
-        print(self.get_object().receiver, self.get_object().sender, profile)
         if not request.user.is_authenticated:
             # This will redirect to the login view
             return self.handle_no_permission()
         if self.get_object().receiver != profile:
             if self.get_object().sender != profile:
-            # Redirect the user to somewhere else - add your URL here
+                # Redirect the user to somewhere else - add your URL here
                 return redirect('profile')
 
         # Checks pass, let http method handlers process the request
@@ -119,23 +129,24 @@ class UpdateFriendRequestView(AccessMixin, UpdateView):
 
     def get_object(self):
         return get_object_or_404(FriendRequest, id=self.kwargs['pk'])
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page'] = 'Update Friend Request'
         if self.get_object().sender.user == self.request.user:
             context['delete_link'] = True
             context['delete_pk'] = self.kwargs['pk']
-
         return context
 
+
 class DeleteFriendRequestView(AccessMixin, DeleteView):
+    '''Delete friend request view'''
     model = FriendRequest
     template_name = 'home/delete.html'
-    
+
     def get_success_url(self):
         return reverse_lazy('profile')
-    
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             # This will redirect to the login view
@@ -146,32 +157,38 @@ class DeleteFriendRequestView(AccessMixin, DeleteView):
         # Checks pass, let http method handlers process the request
         return super().dispatch(request, *args, **kwargs)
 
+
 class FriendRequestListView(ListView):
+    '''Friend request list view'''
     model = FriendRequest
     template_name = 'profiles/requests.html'
     context_object_name = 'list_object'
     paginate_by = 12
 
     def get_queryset(self):
-        q = self.request.GET.get('q') if self.request.GET.get('q') != None else ''
+        q = self.request.GET.get(
+            'q'
+            ) if self.request.GET.get('q') is not None else ''
         queryset = FriendRequest.objects.filter(
-            Q(sender__user=self.request.user) | 
-            Q (receiver__user=self.request.user)
+            Q(sender__user=self.request.user) |
+            Q(receiver__user=self.request.user)
             ).filter(
-                Q(sender__first_name__icontains=q) | 
-                Q(sender__last_name__icontains=q) | 
-                Q (receiver__first_name__icontains=q) |
-                Q (receiver__last_name__icontains=q) |
-                Q (receiver__user__username__icontains=q)
+                Q(sender__first_name__icontains=q) |
+                Q(sender__last_name__icontains=q) |
+                Q(receiver__first_name__icontains=q) |
+                Q(receiver__last_name__icontains=q) |
+                Q(receiver__user__username__icontains=q)
             )
         return queryset
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search'] = True
         return context
 
+
 class RemoveFriendView(AccessMixin, TemplateView, FormMixin):
+    '''Remove Friend view'''
     template_name = 'home/delete.html'
     success_url = reverse_lazy('profile')
     form_class = FriendRequestForm
@@ -182,12 +199,11 @@ class RemoveFriendView(AccessMixin, TemplateView, FormMixin):
         if not request.user.is_authenticated:
             # This will redirect to the login view
             return self.handle_no_permission()
-        if not profile in view_profile.get_friends():
+        if profile not in view_profile.get_friends():
             # Redirect the user to somewhere else - add your URL here
             return redirect('profile')
         # Checks pass, let http method handlers process the request
         return super().dispatch(request, *args, **kwargs)
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -195,31 +211,32 @@ class RemoveFriendView(AccessMixin, TemplateView, FormMixin):
         context['page'] = 'Remove'
 
         return context
-    
+
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
-    
+
     def form_valid(self, form):
         profile = Profile.objects.get(user__id=self.request.user.id)
         view_profile = Profile.objects.get(id=self.kwargs['pk'])
         profile.unfriend(view_profile)
-            
-
         return super().form_valid(form)
 
 
 class SessionRequestListView(ListView):
+    '''Session request list view'''
     model = SessionInvite
     template_name = 'profiles/session_invites.html'
     context_object_name = 'list_object'
     paginate_by = 12
 
     def get_queryset(self):
-        q = self.request.GET.get('q') if self.request.GET.get('q') != None else ''
+        q = self.request.GET.get(
+            'q'
+            ) if self.request.GET.get('q') is not None else ''
         queryset = SessionInvite.objects.filter(
             Q(receiver__user=self.request.user)
             ).filter(
@@ -228,22 +245,23 @@ class SessionRequestListView(ListView):
                 Q(session__admin__username__icontains=q)
             )
         return queryset
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search'] = True
         return context
 
+
 class UserSessionInviteView(AccessMixin, UpdateView):
+    '''Update session invite view'''
     model = SessionInvite
     template_name = 'home/form.html'
     success_url = reverse_lazy('profile')
 
     def get_form_class(self):
         form_class = SessionInviteUpdateForm
-        if self.get_object().inbound == True:
+        if self.get_object().inbound is True:
             form_class = SessionJoinForm
-        
         return form_class
 
     def dispatch(self, request, *args, **kwargs):
@@ -260,23 +278,25 @@ class UserSessionInviteView(AccessMixin, UpdateView):
 
     def get_object(self):
         return get_object_or_404(SessionInvite, id=self.kwargs['pk'])
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page'] = 'Update Session Invite'
-        if self.get_object().inbound == True:
+        if self.get_object().inbound is True:
             context['delete_link'] = True
             context['delete_pk'] = self.kwargs['pk']
 
         return context
 
+
 class DeleteUserSessionInviteView(AccessMixin, DeleteView):
+    '''Delete session invite view'''
     model = SessionInvite
     template_name = 'home/delete.html'
-    
+
     def get_success_url(self):
         return reverse_lazy('profile')
-    
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             # This will redirect to the login view
