f3b11a7607596da564cb8903c0bc2df468426ecc
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-05-01 10:01:22 +0100

    Add pep8 validation

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,8 @@
+'''Admin for profile app'''
 from django.contrib import admin
 from .models import Profile, FriendRequest, SessionInvite
 
-# Register your models here.
+
 admin.site.register(Profile)
 admin.site.register(FriendRequest)
 admin.site.register(SessionInvite)
