07babbe669eb4ff44688787321d19f3c1c449579
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-05-04 08:33:10 +0100

    Add static serving

--- badminton/urls.py
+++ badminton/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     path('accounts/profile/', include('profiles.urls')),
     path('', include('home.urls')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
